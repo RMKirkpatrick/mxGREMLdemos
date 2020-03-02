106aa987cf32d35d5893e0717a310925e4aa6fd6
RMKirkpatrick <rkirkpatrick2@vcu.edu>
2020-03-02 15:54:04 -0700

    IndePthwy.R : use Haseman-Elston regression to get start values. The model now converges in a reasonable length of time.

--- IndePthwy.R
+++ IndePthwy.R
@@ -2,14 +2,14 @@
 # Licensed under CC BY 4.0 <http://creativecommons.org/licenses/by/4.0/>
 
 # Demonstration of fitting an independent-pathway model.
-# This is still a work-in-progress; it does not converge in a reasonable amount of time.
+# Parameterization: loadings onto the IPs are free, IP's variances fixed to 1.0.
 
 set.seed(1705160)
 library(mvtnorm)
 library(Matrix)
 library(OpenMx)
 options(mxCondenseMatrixSlots=TRUE)
-mxOption(NULL,"Default optimizer","NPSOL")
+mxOption(NULL,"Default optimizer","SLSQP")
 mxOption(NULL,"Analytic Gradients","Yes")
 
 #With more threads, the job will run more quickly, but will require more memory:
@@ -57,6 +57,40 @@ z2mvnorm <- function(z,ev,const){
 	R <- t(ev$vectors %*% (t(ev$vectors) * sqrt(ev$values*const)))
 	retval <- matrix(z,nrow=1) %*% R
 	return(retval)
+}
+
+#Function to obtain start values for heritabilities from Haseman-Elston regression:
+HEstartvals <- function(yy, GRM, bound=TRUE){
+	#Standardized phenotype:
+	yy1 <- (yy-mean(yy))/sd(yy)
+	#Sample size, inferred from order of GRM:
+	N <- nrow(GRM)
+	#Upper triangle of GRM:
+	U <- matrix(NA_real_,N,N)
+	U[!lower.tri(U,diag=T)] <- GRM[!lower.tri(GRM,diag=T)]
+	#Vector of off-diagonal GRM coefficients:
+	xx <- rep(NA_real_, N*(N-1)/2)
+	#Vectors to hold cross-products of participants' standardized phenotypes:
+	yy1.2 <- rep(NA_real_, N*(N-1)/2)
+	#Loop to populate vectors to be used in HE regression:
+	sofar <- 1
+	for(i in 1:(N-1)){
+		yy1.2[sofar:(sofar+N-1-i)] <- yy1[i]*yy1[(i+1):N]
+		xx[sofar:(sofar+N-1-i)] <- U[i,][!is.na(U[i,])]
+		sofar <- sofar+N-i
+		#print(i)
+	}
+	#Should be no more NAs:
+	if(is.na(sum(is.na(yy1.2)))){stop("NAs detected")}
+	if(is.na(sum(is.na(xx)))){stop("NAs detected")}
+	#HE regression; slope estimates observed-scale heritability:
+	her <- lm(yy1.2~xx)$coefficients[2]
+	if(bound){
+		#Enforce bounds on observed-scale heritability estimates:
+		her <- ifelse(her<0, 0, her)
+		her <- ifelse(her>0.9999, 0.9999, her)
+	}
+	return(her)
 }
 
 #Generate msnps SNPs.  SNPs are in linkage equilibrium, with MAF in interval [0.05,0.5]:
@@ -108,20 +142,22 @@ widedata <- cbind(Y1,Y2,Y3,Y4,X1,X2)
 colnames(widedata) <- c("y1","y2","y3","y4","x1","x2")
 rm(Y1,Y2,Y3,Y4,X1,X2,Ac,Ec,A1,A2,A3,A4,E1,E2,E3,E4); gc()
 
-# TODO: Get start values from residual variances & HE regression.
+#Get start values from residual variances & HE regression:
+resvarcs <- rep(NA_real_,4)
+h2starts <- rep(NA_real_,4)
+for(i in 1:4){
+	resvarcs[i] <- var(lm(widedata[,i]~widedata[,5]+widedata[,6])$residuals)
+	h2starts[i] <- HEstartvals(widedata[,i],GRM,T)
+}
 
 # Begin assembling mxGREML independent-pathway model: #########
 
 #This is the MxExpectationGREML object.  The value of argument 'Xvars' is telling OpenMx to regress all five phenotypes onto both covariates:
 xpec <- mxExpectationGREML(V="V",yvars=c("y1","y2","y3","y4"),Xvars=list(c("x1","x2")),blockByPheno=T)
 
-#Custom compute plan to use NPSOL, with analytic gradients but no warm start:
+#Custom compute plan:
 plan <- mxComputeSequence(
 	steps=list(
-		#mxComputeNewtonRaphson(verbose=5L),
-		#mxComputeOnce("fitfunction", "hessian"),
-		#mxComputeNelderMead(centerIniSimplex=T,fTolProx=1e-7,xTolProx=1e-7),
-		#mxComputeGradientDescent(engine="CSOLNP",useGradient=T,verbose=0L,tolerance=1e-7,maxMajorIter=2L),
 		mxComputeGradientDescent(engine="SLSQP",useGradient=T,verbose=5L),
 		mxComputeOnce("fitfunction", c("gradient","hessian")),
 		mxComputeStandardError(),
@@ -140,20 +176,20 @@ ipmod <- mxModel(
 	#Start values are such that the variance in each trait is one-quarter common A, common E, unique A, and unique E:
 	mxMatrix(
 		type="Full",nrow=4,ncol=1,free=c(T,T,T,T),
-		values=sqrt(apply(widedata,2,var,na.rm=T)[1:4]/4),
+		values=sqrt(h2starts*resvarcs/2),
 		labels=c("lac1","lac2","lac3","lac4"), name="LA"),
 	mxMatrix(
 		type="Full",nrow=4,ncol=1,free=c(T,T,T,T),
-		values=sqrt(apply(widedata,2,var,na.rm=T)[1:4]/4),
+		values=sqrt((resvarcs - h2starts*resvarcs)/2),
 		labels=c("lec1","lec2","lec3","lec4"), name="LE"),
-	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=var(widedata[,"y1"],na.rm=T)/4,labels="vau1",lbound=0,name="Vau1"),
-	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=var(widedata[,"y2"],na.rm=T)/4,labels="vau2",lbound=0,name="Vau2"),
-	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=var(widedata[,"y3"],na.rm=T)/4,labels="vau3",lbound=0,name="Vau3"),
-	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=var(widedata[,"y4"],na.rm=T)/4,labels="vau4",lbound=0,name="Vau4"),
-	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=var(widedata[,"y1"],na.rm=T)/4,labels="veu1",lbound=0.0001,name="Veu1"),
-	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=var(widedata[,"y2"],na.rm=T)/4,labels="veu2",lbound=0.0001,name="Veu2"),
-	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=var(widedata[,"y3"],na.rm=T)/4,labels="veu3",lbound=0.0001,name="Veu3"),
-	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=var(widedata[,"y4"],na.rm=T)/4,labels="veu4",lbound=0.0001,name="Veu4"),
+	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=h2starts[1]*resvarcs[1]/2,labels="vau1",lbound=0,name="Vau1"),
+	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=h2starts[2]*resvarcs[2]/2,labels="vau2",lbound=0,name="Vau2"),
+	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=h2starts[3]*resvarcs[3]/2,labels="vau3",lbound=0,name="Vau3"),
+	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=h2starts[4]*resvarcs[4]/2,labels="vau4",lbound=0,name="Vau4"),
+	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=(resvarcs[1] - h2starts[1]*resvarcs[1])/2,labels="veu1",lbound=0.0001,name="Veu1"),
+	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=(resvarcs[2] - h2starts[2]*resvarcs[2])/2,labels="veu2",lbound=0.0001,name="Veu2"),
+	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=(resvarcs[3] - h2starts[3]*resvarcs[3])/2,labels="veu3",lbound=0.0001,name="Veu3"),
+	mxMatrix(type="Full",nrow=1,ncol=1,free=T,values=(resvarcs[4] - h2starts[4]*resvarcs[4])/2,labels="veu4",lbound=0.0001,name="Veu4"),
 	
 	#Some matrices constant w/r/t free parameters:
 	mxMatrix(type="Symm",nrow=N,free=F,values=GRM,name="A"),
